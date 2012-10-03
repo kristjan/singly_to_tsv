679713c28b4b73266a1c133e91f73b72fdffab2d
Kristján Pétursson <kristjan@gmail.com>
2012-10-03 12:15:55 -0700

    Output to TSV

--- fetch.rb
+++ fetch.rb
@@ -1,5 +1,6 @@
 #! /usr/bin/env ruby
 
+require 'csv'
 require 'httparty'
 
 args = ARGV.to_a
@@ -7,16 +8,15 @@ if args.length < 2
   puts "USAGE: fetch <path> <singly.field>:<column_name> [...]"
 end
 
-puts args.inspect
 path = args[0]
-puts "Getting #{args[0]}"
+$stderr.puts "Getting #{args[0]}"
 
 map = {}
 args[1..-1].each do |field|
   singly, column = field.split(':')
   map[singly] = column
 end
-puts "Converting #{map.inspect}"
+$stderr.puts "Converting #{map.inspect}"
 
 API_HOST = 'https://api.singly.com'
 
@@ -27,4 +27,11 @@ data = HTTParty.get(API_HOST + path, {
   }
 })
 
-puts data
+out = CSV.generate(col_sep: "\t") do |csv|
+  columns = map.keys
+  csv << columns.map{|col| map[col]}
+  data.each do |datum|
+    csv << columns.map{|col| datum[col]}
+  end
+end
+puts out
